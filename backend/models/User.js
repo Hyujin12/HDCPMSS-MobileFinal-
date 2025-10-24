12a7d96714f40e870c54d6307d66b11e17889245
Hyujin12 <eugenedianito@gmai.com>
2025-10-24 15:07:34 +0800

    PUSHED

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  contactNumber: { type: Number, required: true },
+  username: { type: String, required: true, trim: true },
+  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+  contactNumber: { type: String, required: true, trim: true }, // change Number -> String
   password: { type: String, required: true },
   isVerified: { type: Boolean, default: false },
-  verificationCode: String,
-  codeExpires: Date,
-});
+  verificationCode: { type: String },
+  codeExpires: { type: Date },
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
